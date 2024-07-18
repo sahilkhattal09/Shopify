c42a41c3915f66a8bc2ed30d0d1ccc3f9aa8db6e
sahilkhattal09 <sahilkhattal7@gmail.com>
2024-07-18 22:41:15 +0530

    changed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./Pages/Signup";
 
+import Login from "./Pages/LogIn";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Signup />}>
-          <Route path="blogs" element={<Signup />} />
-        </Route>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
