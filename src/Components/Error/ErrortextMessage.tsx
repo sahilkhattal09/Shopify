0ac0f3779c0013016854482ca5da91a68383d832
sahilkhattal09 <sahilkhattal7@gmail.com>
2024-08-11 12:17:44 +0530

    feat(error):created error component which will display error

---
+++ src/Components/Error/ErrortextMessage.tsx
@@ -0,0 +1,21 @@
+interface ErrorMessageProps {
+  error?: boolean;
+  message?: string | undefined;
+  className?: string;
+}
+
+export default function ErrorMessage({
+  error,
+  message,
+  className,
+}: ErrorMessageProps) {
+  return (
+    <div
+      className={`text-sm ${
+        error ? "text-red-600" : "text-black"
+      } ${className}`}
+    >
+      {message}
+    </div>
+  );
+}
