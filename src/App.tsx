5d9ab3b004d5a4abd76394dd9974399f718f5bfd
sahilkhattal09 <sahilkhattal7@gmail.com>
2024-08-11 12:21:27 +0530

    tailwind css  configurtion updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./Pages/Signup";
 
 import Login from "./Pages/LogIn";
+import Header from "./Components/UI/Header/Header";
 
 export default function App() {
   return (
@@ -9,8 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
-
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Header />} />
       </Routes>
     </BrowserRouter>
   );
