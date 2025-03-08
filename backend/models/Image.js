cd166b6e5d623ef380ba651f96aa742a07e7bda3
sahilkhattal09 <sahilkhattal7@gmail.com>
2025-03-08 15:58:51 +0530

    feat(image):added category in imageschema

--- backend/models/Image.js
+++ backend/models/Image.js
@@ -5,6 +5,7 @@ const imageSchema = new mongoose.Schema({
   data: Buffer, // This will be your image data, in base64 or binary format
   contentType: String, // Type of the image (e.g., 'image/jpeg', 'image/png')
   price: Number, // The price associated with the product
+  category: String,
 });
 
 const Image = mongoose.model("Image", imageSchema);
